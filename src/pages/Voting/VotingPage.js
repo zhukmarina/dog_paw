df477bfc147deccf2b129896fc65d62575bf4ece
Marina Zhukova <marinazhukova@MacBook-Air-Marina.local>
2024-01-03 22:50:36 +0200

    add auth

--- src/pages/Voting/VotingPage.js
+++ src/pages/Voting/VotingPage.js
@@ -3,28 +3,29 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchDogsForVoting, addVote } from '../../appStore/actionCreators/voteActionCreators'; 
 import VoteImage from '../../components/VoteImage/VoteImage';
 import styles from './VotingPage.module.scss';
+import { useUserAuth } from '../../context/UserAuthContext';
 
 const VotingPage = () => {
   const dispatch = useDispatch();
   const [currentDogIndex, setCurrentDogIndex] = useState(0);
   const { votingDogs, isLoading } = useSelector((state) => state.votingReducer);
+  const { user } = useUserAuth();
 
   useEffect(() => {
     dispatch(fetchDogsForVoting());
   }, [dispatch]);
 
   const handleVote = (liked) => {
+
+    
     const currentDogId = votingDogs[currentDogIndex]?.id;
 
     if (currentDogId) {
-      const voteObject = {
-        dogId: currentDogId,
-        value: liked ? 1 : 0,
-      };
-      console.log('Vote Object:', voteObject);
-      dispatch(addVote(currentDogId, liked)); // Виклик addVote замість voteForDog
+      const value = liked ? 1 : -1;
+      dispatch(addVote(user.uid, currentDogId, value));
     }
   };
+
 
   const nextImage = () => {
     setCurrentDogIndex((prevIndex) => prevIndex + 1);
@@ -35,7 +36,7 @@ const VotingPage = () => {
       {isLoading ? (
         <p>Loading...</p>
       ) : votingDogs.length > 0 && currentDogIndex < votingDogs.length ? (
-        <VoteImage imageUrl={votingDogs[currentDogIndex].url} onVote={handleVote} onNextImage={nextImage} />
+        <VoteImage imageUrl={votingDogs[currentDogIndex].url} onVote={handleVote} onNextImage={nextImage}  />
       ) : (
         <p>Ви проголосували за всіх собак!</p>
       )}
